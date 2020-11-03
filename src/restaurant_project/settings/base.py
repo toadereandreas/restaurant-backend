3955559a3a52df8d779cd174d5bd4d6ab2927dff
SuciuPatrick <patrick.suciu97@gmail.com>
2020-11-03 20:40:16 +0200

    added cors policy

--- src/restaurant_project/settings/base.py
+++ src/restaurant_project/settings/base.py
@@ -36,13 +36,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'corsheaders',
     'restaurant_entities.apps.RestaurantEntitiesConfig',
     'graphql_auth',
     'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
     'restaurant_entities.users',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
