81e6a76638fdc1ba90b64c83e7453f6e4c1020e1
stiubeiciprian <stiubeiciprian@gmail.com>
2020-11-30 15:50:58 +0200

    Configure asgi app

--- src/restaurant_project/asgi.py
+++ src/restaurant_project/asgi.py
@@ -10,11 +10,32 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from django.urls import path 
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_project.settings.base')
 django.setup()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+from graphene_subscriptions.consumers import GraphqlSubscriptionConsumer
+
+
+class CustomProtocolTypeRouter(ProtocolTypeRouter):
+    """
+    Overrided base class's __call__ method to support python socketio 4.2.0 and daphne 2.3.0
+    """
+    def __call__(self, scope, *args):
+        if scope["type"] in self.application_mapping:
+            handlerobj = self.application_mapping[scope["type"]](scope)
+            if args:
+                return handlerobj(*args)
+            return handlerobj
+        raise ValueError(
+            "No application configured for scope type %r" % scope["type"]
+        )
+
+
+application = CustomProtocolTypeRouter({
+     "websocket": URLRouter([
+        path('graphql/', GraphqlSubscriptionConsumer)
+    ]),
 })
--- src/restaurant_project/settings/development_settings.py
+++ src/restaurant_project/settings/development_settings.py
@@ -17,3 +17,15 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_HOST = os.environ.get('AWS_S3_HOST')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
