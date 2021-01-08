98445501fa9c8157b644a23dd3e4a8f4e7929a9c
Cippry <stiubeiciprian@gmail.com>
2021-01-08 23:31:10 +0200

    Remove redis from schema

--- src/restaurant_graphql/schema/types/order.py
+++ src/restaurant_graphql/schema/types/order.py
@@ -1,7 +1,7 @@
 from .base import RestaurantObjectType
 import graphene
 from restaurant_entities.models.order import Order, Serving
-import redis
+
 
 class OrderNode(RestaurantObjectType):
     class Meta:
@@ -16,21 +16,12 @@ class OrderNode(RestaurantObjectType):
 
     id = graphene.ID()
     serving_id = graphene.ID()
-    # locked = graphene.Boolean()
 
     def resolve_id(self, info):
         return Order.get_pk(gid=self.gid)
 
     def resolve_serving_id(self, info):
         return Serving.get_pk(gid=self.serving.gid)
-
-    # def resolve_locked(self, info):
-
-    #     redis_instance = redis.StrictRedis(host='localhost',
-    #                                        port=6379, db=0)
-    #     if str(redis_instance.get(str(self.gid))) == "b'True'":
-    #         return True
-    #     return False
 
 
 class OrderList(graphene.ObjectType):
--- src/restaurant_graphql/schema/types/serving.py
+++ src/restaurant_graphql/schema/types/serving.py
@@ -2,7 +2,7 @@ from .base import RestaurantObjectType
 import graphene
 from restaurant_entities.models.order import Serving
 from restaurant_entities.users.models import CustomUser
-import redis
+
 
 class ServingNode(RestaurantObjectType):
     class Meta:
@@ -17,22 +17,12 @@ class ServingNode(RestaurantObjectType):
 
     id = graphene.ID()
     user_id = graphene.ID()
-    # called = graphene.Boolean()
-
 
     def resolve_id(self, info):
         return Serving.get_pk(gid=self.gid)
 
     def resolve_user_id(self, info):
         return CustomUser.objects.get(username=self.user.username).pk
-
-    # def resolve_called(self, info):
-    #     #return False
-    #     redis_instance = redis.StrictRedis(host='localhost',
-    #                                        port=6379, db=0)
-    #     if str(redis_instance.get(str(self.gid))) == "b'True'":
-    #         return True
-    #     return False
 
 
 class ServingList(graphene.ObjectType):
