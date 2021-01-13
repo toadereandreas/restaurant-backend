087230a44073810c4315c1f45d5b8b2bbec93528
Cippry <stiubeiciprian@gmail.com>
2021-01-13 22:35:04 +0200

    Add createOrderFrontend mutation

--- src/restaurant_graphql/schema/order.py
+++ src/restaurant_graphql/schema/order.py
@@ -1,7 +1,7 @@
 import graphene
 from rx import Observable
 
-from restaurant_entities.models.order import Order
+from restaurant_entities.models.order import Order, Serving
 from restaurant_graphql.schema.types.base import ErrorType
 from restaurant_graphql.schema.types.order import (
     OrderList,
@@ -12,13 +12,13 @@ from restaurant_graphql.schema.helpers import get_errors
 from restaurant_graphql.forms.order import OrderForm
 from graphql.error import GraphQLError
 
+import random
 
 
 class Query(graphene.ObjectType):
     orders = graphene.Field(
-        OrderList) #,
-        #id=graphene.ID(),
-    #)
+        OrderList)
+
     order = graphene.Field(OrderNode, id=graphene.ID(required=True))
 
     def resolve_order(self, info, id):
@@ -27,6 +27,47 @@ class Query(graphene.ObjectType):
     def resolve_orders(self, info, **kwargs):
         qs = Order.objects.all()
         return OrderList(qs)
+
+
+class CreateOrderFrontendMutation(graphene.Mutation):
+    class Arguments:
+        serving_code = graphene.String(required=True, description="Serving code")
+
+    order = graphene.Field(
+        OrderNode,
+        description = "Created order."
+    )
+
+    errors = graphene.List(
+        ErrorType,
+        description = 'List of errors that occurred executing the mutation.'
+    )
+
+    class Meta:
+        description = "Creates an order."
+
+    @classmethod
+    def mutate(cls, root, info, serving_code, **kwargs):
+        colors = ["#4C2C69", "#42253B", "#747572", "#E98A15", "#36393B", "#9F4A54","#0E1C36","#048A81","#00ABE7","#4CB5AE"]
+       
+        serving = Serving.objects.get(code=serving_code)
+        
+        # Return an error message if the table is full (no more colors available)
+        if Order.objects.filter(serving=serving).count() >= len(colors):
+            return CreateOrderFrontendMutation(
+                errors=[ErrorType(message="Table is full.")] # TODO: Fix error message
+            )
+
+        random_color = random.choice(colors)
+        while Order.objects.filter(serving=serving, color=random_color).exists():
+            random_color = random.choice(colors)
+
+        order = Order(serving=serving, color=random_color, note='', locked=False)
+        order.save()
+
+        return CreateOrderFrontendMutation(
+            order=order
+        )
 
 
 class CreateOrderMutation(graphene.Mutation):
@@ -64,7 +105,6 @@ class CreateOrderMutation(graphene.Mutation):
         return CreateOrderMutation(
             order=order
         )
-
 
 
 class DeleteOrderMutation(graphene.Mutation):
@@ -123,5 +163,6 @@ class UpdateOrderMutation(graphene.Mutation):
 
 class Mutation(graphene.ObjectType):
     create_order = CreateOrderMutation.Field()
-    delete_order=DeleteOrderMutation.Field()
-    update_order=UpdateOrderMutation.Field()
+    create_order_frontend = CreateOrderFrontendMutation.Field() 
+    delete_order = DeleteOrderMutation.Field()
+    update_order = UpdateOrderMutation.Field()
